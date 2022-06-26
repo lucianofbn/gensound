f3254125d0e419f607998daa19e518a2bfb58335
Luciano Ferreira <lfbnwork@gmail.com>
2022-06-26 21:30:58 +0200

    change repository

--- src/model/repository.js
+++ src/model/repository.js
@@ -1,3 +1,3 @@
-//export const server = "https://gensound.web.app"
-export const server = "http://localhost:8080"
+export const server = "https://gensound-webapp.web.app"
+//export const server = "http://localhost:8080"
 export const endpoint_generatemusic = "/generatemusic"
